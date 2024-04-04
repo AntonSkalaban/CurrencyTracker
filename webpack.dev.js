51df57f977c71ad601176f695777927f8ce8153c
Anton Skalaban <antonskalaban@MacBook-Air-Anton.local>
2024-04-04 18:41:47 +0300

    feat: configured the project

---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: { port: 3000, open: true, hot: true },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+});
---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: false,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+    }),
+  ],
+});
