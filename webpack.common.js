0595a2678444922ea9a66f01b0ecd786c0db1427
Anton Skalaban <antonskalaban@MacBook-Air-Anton.local>
2024-04-18 00:55:43 +0300

    feat: add public folder

--- webpack.common.js
+++ webpack.common.js
@@ -51,6 +51,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
+      favicon: path.resolve(__dirname, "public", "logo.svg"),
     }),
     new Dotenv(),
   ],
