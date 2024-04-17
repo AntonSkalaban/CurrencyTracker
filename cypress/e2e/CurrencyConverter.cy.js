3748b13f586310681b53f7ff441a9eecbd3e8a9c
Anton Skalaban <antonskalaban@MacBook-Air-Anton.local>
2024-04-17 09:08:30 +0300

    feat: add navigation-test

--- cypress/e2e/CurrencyConverter.cy.js
+++ cypress/e2e/CurrencyConverter.cy.js
@@ -6,15 +6,12 @@ describe("CurrencyConverter Component", () => {
 
   it("should convert currency successfully", () => {
     cy.get('[data-testid="converted-value"]').should("have.text", "1.00");
-
     cy.get('[name="amount"]').type("0");
-
     cy.get('[data-testid="converted-value"]').should("have.text", "10.00");
   });
 
   it("should display error message on API failure", () => {
     cy.get('[data-testid="dropdown"]').click();
-
     cy.contains("EUR");
   });
 });
